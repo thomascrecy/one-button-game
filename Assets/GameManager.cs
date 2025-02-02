ee145bf98b1aa447f8c8928a33baadfd75a09195
Thomas <126467625+thomascrecy@users.noreply.github.com>
2025-02-02 13:37:40 +0100

    Game over + score

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro; 
 
 public class GameManager : MonoBehaviour
 {
     public GameObject platformPrefab;
-
     public int platformCount = 300;
+    public Transform player; 
+    public GameObject gameOverUI; 
+    public GameObject RestartButton; 
+    public TextMeshProUGUI scoreText; 
+
+    private float minY; 
+    private float highestY = 0f;
+    private int score = 0;
 
     void Start()
     {
@@ -14,9 +23,39 @@ public class GameManager : MonoBehaviour
 
         for (int i = 0; i < platformCount; i++)
         {
-            spawnPosition.y += Random.Range(.5f, 2f);
-            spawnPosition.x = Random.Range(-5f, 5f);
+            spawnPosition.y += Random.Range(2f, 3f);
+            spawnPosition.x = Random.Range(-3f, 5f);
             Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
         }
+
+        minY = player.position.y - 3f; 
+    }
+
+    void Update()
+    {
+        if (player.position.y > highestY)
+        {
+            highestY = player.position.y;
+            score = Mathf.FloorToInt(highestY); 
+            scoreText.text = "Score: " + score.ToString();
+        }
+
+        if (player.position.y < minY)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        gameOverUI.SetActive(true); 
+        RestartButton.SetActive(true); 
+        Time.timeScale = 0f; 
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
     }
 }
